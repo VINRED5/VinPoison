7963cc68a46e71f1047ef9743f3156a989318ab6
VINRED5 <lukinihkirill85@gmail.com>
2023-06-07 15:18:52 +0400

    Initial commit

---
+++ src/main/java/me/vinred5/vinred5/VinPoison.java
@@ -0,0 +1,17 @@
+package me.vinred5.vinred5;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VinPoison extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
