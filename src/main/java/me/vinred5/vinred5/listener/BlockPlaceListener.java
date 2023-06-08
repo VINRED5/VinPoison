186a79619ca820b7f2cbeeb7a1b1d02f5cfcba82
VINRED5 <lukinihkirill85@gmail.com>
2023-06-08 14:23:22 +0400

    nice

--- src/main/java/me/vinred5/vinred5/listener/BlockPlaceListener.java
+++ src/main/java/me/vinred5/vinred5/listener/BlockPlaceListener.java
@@ -9,36 +9,48 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class BlockPlaceListener implements Listener {
+
+    VinPoison instance = VinPoison.getInstance();
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         Player player = event.getPlayer();
         Block block = event.getBlock();
-        Location loc = block.getLocation().add(0, -1, 0);
-        Material campfireCheck = loc.getBlock().getType();
-        Material campfire = Material.valueOf("CAMPFIRE");
-
-        VinPoison instance = VinPoison.getInstance();
 
         if (event.canBuild() && !event.isCancelled()) {
-            List<String> blockInfo = Arrays.asList(
-                    "&fВы установили блок: &e" + block.getType(),
-                    "&fКоординаты: &e" + block.getLocation().getX() + ", " + block.getLocation().getY() + ", " + block.getLocation().getZ(),
-                    "&fСнизу: &e" + campfireCheck
+            List<String> blockInfo = Collections.singletonList(
+                    "&fВы установили блок: &e" + block.getType()
             );
-
             blockInfo.forEach(s -> player.sendMessage(Component.text(instance.color(s))));
+            if (block.getType() == Material.CAMPFIRE) {
+                checkCampfire(block, player);
+            } else if (block.getType() == Material.CAULDRON) {
+                checkCauldron(block, player);
+            }
         }
-        if (campfireCheck == campfire) {
-            player.sendMessage(Component.text("&dУсловие выполненно"));
-        } else player.sendMessage(Component.text("&dУсловие не выполненно"));
+    }
 
+    public void checkCampfire(Block block, Player player) {
+        Location loc = block.getLocation().add(0, 1, 0);
+        Material checkCauldron = loc.getBlock().getType();
+        if (checkCauldron == Material.CAULDRON) {
+            List<String> yes = Collections.singletonList("&dУсловие выполнено: над костром находиться котёл");
+            yes.forEach(s -> player.sendMessage(Component.text(instance.color(s))));
+        }
+    }
+
+    public void checkCauldron(Block block, Player player) {
+        Location loc = block.getLocation().add(0, -1, 0);
+        Material checkCampfire = loc.getBlock().getType();
+        if (checkCampfire == Material.CAMPFIRE) {
+            List<String> yes = Collections.singletonList("&dУсловие выполнено: под котлом находиться костёр");
+            yes.forEach(s -> player.sendMessage(Component.text(instance.color(s))));
+        }
     }
 
 }
