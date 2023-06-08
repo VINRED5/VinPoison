b1087a031a8cc3fc8282a15f6d0abae46939e16a
VINRED5 <lukinihkirill85@gmail.com>
2023-06-08 13:01:24 +0400

    Test

--- src/main/java/me/vinred5/vinred5/VinPoison.java
+++ src/main/java/me/vinred5/vinred5/VinPoison.java
@@ -1,20 +1,49 @@
 package me.vinred5.vinred5;
 
+import me.vinred5.vinred5.listener.BlockPlaceListener;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.List;
 
 public final class VinPoison extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
-        Bukkit.getServer().getConsoleSender().sendMessage(Component.text(Color.BLUE + "Плагин включился"));
+    private static VinPoison instance;
 
+    public static VinPoison getInstance() {
+        return instance;
     }
 
     @Override
+    public void onEnable() {
+        instance = this;
+        console(true);
+        Bukkit.getPluginManager().registerEvents(new BlockPlaceListener(), instance);
+    }
+
+
+    @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        console(false);
+    }
+
+    public String color(String text) {
+        return ChatColor.translateAlternateColorCodes('&', text);
+    }
+
+    private void console(boolean enable) {
+        String status = enable ? "Включён" : "Выключен";
+
+        List<String> messages = Arrays.asList(
+                instance.getDescription().getFullName() + instance.getDescription().getAuthors(),
+                "-------------------",
+                "Статус: " + status,
+                "Версия: " + instance.getDescription().getVersion()
+        );
+
+        messages.forEach(str -> Bukkit.getConsoleSender().sendMessage(Component.text(color(str))));
     }
 }
---
+++ src/main/java/me/vinred5/vinred5/listener/BlockPlaceListener.java
@@ -0,0 +1,31 @@
+package me.vinred5.vinred5.listener;
+
+import me.vinred5.vinred5.VinPoison;
+import net.kyori.adventure.text.Component;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BlockPlaceListener implements Listener {
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+
+        VinPoison instance = VinPoison.getInstance();
+
+        if (event.canBuild() && !event.isCancelled()) {
+            List<String> blockInfo = Collections.singletonList(
+                    "&fВы установили блок: " + block.getLocation().getX() + ", " + block.getLocation().getY() + ", " + block.getLocation().getZ()
+            );
+
+            blockInfo.forEach(s -> player.sendMessage(Component.text(instance.color(s))));
+        }
+    }
+}
